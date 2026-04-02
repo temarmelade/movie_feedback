31889adf33182b4d917191ab5d575c17d66938c5
Asanov Temirlan <temirlan.asanov@alatoo.edu.kg>
2026-04-02 20:56:46 +0600

    creating project

---
+++ src/main/java/org/example/movie_feedback/MovieFeedbackApplication.java
@@ -0,0 +1,13 @@
+package org.example.movie_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFeedbackApplication.class, args);
+    }
+
+}
