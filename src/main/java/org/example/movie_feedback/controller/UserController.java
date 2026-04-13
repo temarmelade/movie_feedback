f4208760525611094010bc4e67feb9490b1b1c68
temarmelade <temirlan.asanov@alatoo.edu.kg>
2026-04-13 22:47:26 +0600

    UserController is updated, added new requests

--- src/main/java/org/example/movie_feedback/controller/UserController.java
+++ src/main/java/org/example/movie_feedback/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.movie_feedback.dto.UserDto;
 import org.example.movie_feedback.model.User;
 import org.example.movie_feedback.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +21,14 @@ public class UserController {
     public List<UserDto> getUsers() {
         return userService.getUsers();
     }
+    @GetMapping("{userId}")
+    public UserDto getUserById(@PathVariable int userId) {
+        return userService.getUserById(userId);
+    }
     @PostMapping
-    public Optional<UserDto> createUser(@RequestBody UserDto userDto) {
-        return null;
-    };
+    public HttpStatus createUser(@RequestBody UserDto userDto) {
+        userService.addUser(userDto);
+        return HttpStatus.CREATED;
+    }
+
 }
